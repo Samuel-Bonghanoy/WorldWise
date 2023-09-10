31e4d039a15827c202f0e4d2f628951b11d944c5
sethonne <20103261@usc.edu.ph>
2023-09-11 00:07:19 +0800

    feat: added Pricing component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Homepage from "./pages/Homepage";
+import Pricing from "./pages/Pricing";
+
 function App() {
   return (
-    <>
-      <div></div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
