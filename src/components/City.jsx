e851d624f351f27799dbbf8a198bd60ee7127a74
sethonne <20103261@usc.edu.ph>
2023-09-11 23:31:27 +0800

    feat: implemented search params

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import styles from "./City.module.css";
 
 const formatDate = (date) =>
@@ -10,7 +10,10 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function City() {
-  const x = useParams();
+  const { id } = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
   // TEMP DATA
   const currentCity = {
     cityName: "Lisbon",
@@ -20,7 +23,7 @@ function City() {
   };
 
   const { cityName, emoji, date, notes } = currentCity;
-  return <h1>City</h1>;
+  return <h1>City {id}</h1>;
   // return (
   //   <div className={styles.city}>
   //     <div className={styles.row}>
--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -12,10 +12,13 @@ const formatDate = (date) =>
 // eslint-disable-next-line react/prop-types
 function CityItem({ city }) {
   // eslint-disable-next-line react/prop-types
-  const { cityName, emoji, date, id } = city;
+  const { cityName, emoji, date, id, position } = city;
   return (
     <li>
-      <Link className={styles.cityItem} to={`${id}`}>
+      <Link
+        className={styles.cityItem}
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+      >
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>({formatDate(date)})</time>
