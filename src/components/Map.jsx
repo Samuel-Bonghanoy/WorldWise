1a272daa8dddf6d753a9b59351a9813916ccd80c
sethonne <20103261@usc.edu.ph>
2023-09-15 12:00:22 +0800

    feat: added Map component using react-leaflet library

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,7 +2,7 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import { useState } from "react";
 import "leaflet/dist/leaflet.css";
 import styles from "./Map.module.css";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import { useCities } from "../contexts/CitiesContext";
 
 function Map() {
@@ -11,11 +11,12 @@ function Map() {
   const { cities } = useCities();
 
   const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  const mapLat = searchParams.get("lat");
+  const mapLng = searchParams.get("lng");
   return (
     <div className={styles.mapContainer}>
       <MapContainer
+        // center={[mapLat, mapLng]}
         center={mapPosition}
         zoom={13}
         scrollWheelZoom={true}
@@ -35,9 +36,16 @@ function Map() {
             </Popup>
           </Marker>
         ))}
+        <ChangeCenter position={[mapLat || 40, mapLng || 0]} />
       </MapContainer>
     </div>
   );
+}
+
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
 }
 
 export default Map;
