50982a83d258c8f0b97ff18b0a91a3eba73eecc6
sethonne <20103261@usc.edu.ph>
2023-09-16 23:55:16 +0800

    feat: implemented useReducer hook in citiesContext

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -56,7 +56,7 @@ function reducer(state, action) {
 }
 
 function CitiesProvider({ children }) {
-  const [{ cities, isLoading, currentCity }, dispatch] = useReducer(
+  const [{ cities, isLoading, currentCity, error }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -82,6 +82,8 @@ function CitiesProvider({ children }) {
   }, []);
 
   async function getCity(id) {
+    if (Number(id) === currentCity.id) return;
+
     dispatch({ type: "loading" });
     try {
       const res = await fetch(`${BASE_URL}/cities/${id}`);
@@ -137,6 +139,7 @@ function CitiesProvider({ children }) {
         isLoading,
         currentCity,
         getCity,
+        error,
         createCity,
         deleteCity,
       }}
